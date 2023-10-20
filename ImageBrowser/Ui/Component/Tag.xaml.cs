2c6abed7678ad8c7c638e7808c6953733425186a
David Lefaivre-B <david.lefaivre@hotmail.com>
2023-10-20 14:22:36 -0400

    Add "Tag" component

---
+++ ImageBrowser/Ui/Component/Tag.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ImageBrowser.Ui.Component
+{
+    public partial class Tag : UserControl
+    {
+        public Tag() : this("")
+        {
+        }
+
+        public Tag(string title)
+        {
+            InitializeComponent();
+            Title.Content = title;
+        }
+
+        public delegate void DeletePressed();
+
+        public DeletePressed OnDeletePressed;
+        
+        private  void DeleteTag(object sender, MouseButtonEventArgs e)
+        {
+            OnDeletePressed?.Invoke();
+        }
+    }
+}

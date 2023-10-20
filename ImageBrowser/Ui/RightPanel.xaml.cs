f320e874d6d5e22675ad8f701348f630680002d6
David Lefaivre-B <david.lefaivre@hotmail.com>
2023-10-20 14:31:26 -0400

    Add logic to filter by franchise

--- ImageBrowser/Ui/RightPanel.xaml.cs
+++ ImageBrowser/Ui/RightPanel.xaml.cs
@@ -1,13 +1,16 @@
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using ImageBrowser.Controller;
+using ImageBrowser.Ui.Component;
 
 namespace ImageBrowser.Ui
 {
     public partial class RightPanel
     {
         private ThumbnailsController _thumbnailsController;
-        
+
         public RightPanel()
         {
             InitializeComponent();
@@ -22,10 +25,37 @@ namespace ImageBrowser.Ui
         {
             _thumbnailsController.AddCategory(((CheckBox)sender).Content.ToString());
         }
-        
+
         private void OnCheckBoxUnchecked(object sender, RoutedEventArgs e)
         {
             _thumbnailsController.RemoveCategory(((CheckBox)sender).Content.ToString());
+        }
+
+        private void OnFranchiseKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+                AddFranchise();
+        }
+
+        private void AddFranchise()
+        {
+            CreateNewTag();
+            _thumbnailsController.AddFranchise(FranchiseTextBox.Text);
+
+            FranchiseTextBox.Text = "";
+        }
+
+        private void CreateNewTag()
+        {
+            var tag = new Tag(FranchiseTextBox.Text);
+            tag.OnDeletePressed += () => RemoveFranchise(tag);
+            TagsContainer.Children.Add(tag);
+        }
+
+        private void RemoveFranchise(Tag tag)
+        {
+            TagsContainer.Children.Remove(tag);
+            _thumbnailsController.RemoveFranchise((string)tag.Title.Content);
         }
     }
 }

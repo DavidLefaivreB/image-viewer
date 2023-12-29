504673b0b4fd46b14bb5f8200bb65dde362b4e07
David Lefaivre-B <david.lefaivre@hotmail.com>
2023-12-28 21:16:52 -0500

    Create AlbumItem component (album's entry)

---
+++ ImageBrowser/Ui/Component/AlbumItem.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ImageBrowser.Ui.Component;
+
+public partial class AlbumItem : UserControl
+{
+    public string ThumbnailPath => @"D:\patreon\ayyaSAP\pack57\thumbnail\Kim Possible nsfw.jpg";
+    
+    public AlbumItem()
+    {
+        InitializeComponent();
+        DataContext = this;
+        
+        Background = Brushes.Transparent;
+    }
+}

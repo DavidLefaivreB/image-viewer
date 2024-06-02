2afa2edc670c9f0f2cc95d3be8d7fcf5c45dda48
David Lefaivre-B. <david.lefaivre@hotmail.com>
2024-06-02 11:59:49 -0400

    Support drag n drop of files in gallery view

--- ImageBrowser/Ui/View/Gallery/GalleryView.xaml.cs
+++ ImageBrowser/Ui/View/Gallery/GalleryView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
 using ImageBrowser.ViewModel;
 
 namespace ImageBrowser.Ui.View.Gallery;
@@ -20,5 +22,25 @@ public partial class GalleryView
     private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
     {
         ViewModel = DataContext as GalleryViewModel;
+    }
+
+    private void OnDropPreview(object sender, DragEventArgs e)
+    {
+        if (null != e.Data && e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+        }
+    }
+
+    private void OnPreviewDragOver(object sender, DragEventArgs e)
+    {
+        if (e.Data.GetDataPresent(DataFormats.FileDrop))
+        {
+            e.Effects = DragDropEffects.Copy;
+            e.Handled = true;
+        }
+        else
+        {
+            e.Effects = DragDropEffects.None;
+        }
     }
 }
